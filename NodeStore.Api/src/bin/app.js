ac64ec607e58aa727520d4ba1ca6e6e5f3cc229d
Amaury AC <amaury.costa@hotmail.com>
2020-04-06 14:42:49 -0300

    CRUD Products MVC

--- NodeStore.Api/src/bin/app.js
+++ NodeStore.Api/src/bin/app.js
@@ -1,17 +1,22 @@
 'use strict'
 
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+
+const indexRoute = require('../routes/index-route');
+const productRoute = require('../routes/product-route');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res, next) {
-    res.status(200).send({
-        title: 'Node Store API',
-        version: '0.0.1'
-    }); 
-});
+mongoose.connect('mongodb+srv://nodestoreapiadmin:Admin123@cluster0-ifhyj.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
